dc5026424da520fb5a4cf99f80e68d6f6c5c26a5
IvanGlushkov <VanesaNET@yandex.ru>
2022-04-10 19:31:49 +0700

    Оптимизация кода

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,15 +1,13 @@
 package ru.netology
 
 fun main() {
-    val peoples = "людям"
-    val mans = "человеку"
     var likes = 0
-    fun printEnding(likes: Int): String {
-        if (likes % 10 != 1 || likes%100 == 11) return peoples else return mans
-    }
+
     while (true) {
         likes += 1
         Thread.sleep(500)
-        println("Понравилось " + likes + " " + printEnding(likes))
+
+        var pp = if (likes % 10 != 1 || likes % 100 == 11) "людям" else "человеку"
+        println("Понравилось $likes $pp ")
     }
 }
